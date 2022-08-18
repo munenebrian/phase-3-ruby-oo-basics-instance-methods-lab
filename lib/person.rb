af731f35be6969cadeab1649f4bfd11529d9e580
munenebrian <njerubrianmunene@gmail.com>
2022-08-18 11:02:25 +0300

    commit tree ff60c63c5b7210539bd5febc077c72f2811808a8

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
+brian = Person.new
+brian.talk
+brian.walk
+munene = Person.new
+munene.talk
+munene.walk
